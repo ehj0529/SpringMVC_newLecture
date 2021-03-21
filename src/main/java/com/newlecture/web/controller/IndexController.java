7f08e3597b593e123adfa2b23a578a33d3d3941c
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2021-03-21 16:43:56 +0900

    SpringMVC 화면 Layout페이지 적용, Service 적용 (객체 사용하기, 분리하기 등)

--- src/main/java/com/newlecture/web/controller/IndexController.java
+++ src/main/java/com/newlecture/web/controller/IndexController.java
@@ -12,7 +12,7 @@ public class IndexController implements Controller{
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		
-		ModelAndView mv = new ModelAndView("index"); //dispatcher-servlet에 설정함. 나머지 부분.
+		ModelAndView mv = new ModelAndView("root.index"); //dispatcher-servlet에 설정함. 나머지 부분.
 		mv.addObject("data", "Hello Spring MVC ~~");
 		//mv.setViewName("/WEB-INF/view/index.jsp");
 		//viewResolver 기능을 사용하게 되면 "/WEB-INF/view/.../.jsp"를 생략 할수 있음.
